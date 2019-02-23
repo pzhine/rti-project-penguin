13bb863c9da02bd5a9aa26410936240f3f7f2c3c
msevilla <m.sevillazhou@gmail.com>
2019-02-23 21:03:04 +0100

    Desert: Added a ground to prevent death and slippery edges

---
+++ runner-game/Assets/Scripts/Respawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public float RespawnTime;
+    public Transform RespawnLocation;
+
+    public void StartRespawnCoroutine(GameObject go) {
+        ToggleObjectForRespawn(go, false);
+        go.transform.position = new Vector3(999, -999, 999);
+
+        StartCoroutine(RespawnCoroutine(go));
+    }
+
+    IEnumerator RespawnCoroutine(GameObject go) {
+        yield return new WaitForSeconds(RespawnTime);
+
+        ToggleObjectForRespawn(go, true);
+        go.transform.position = RespawnLocation.position;
+    }
+
+    void ToggleObjectForRespawn(GameObject go, bool toggle) {
+        go.GetComponent<MeshRenderer>().enabled = toggle;
+        go.GetComponent<CharacterController>().enabled = toggle;
+    }
+
+}
